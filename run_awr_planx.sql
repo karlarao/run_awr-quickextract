0f7f31ae24eaa232b6ef3bbbd2a37844ff62ff58
karlarao <karlarao@gmail.com>
2019-02-08 16:29:54 -0500

    fixed filters

--- run_awr_planx.sql
+++ run_awr_planx.sql
@@ -12,12 +12,14 @@ COL current_time NEW_V current_time FOR A15;
 SELECT 'current_time: ' x, TO_CHAR(SYSDATE, 'YYYYMMDD_HH24MISS') current_time FROM DUAL;
 
 spool awr_planx-&_instname-&current_time..sql
-select distinct '@planx Y ' || sql_id from 
+select distinct '@planx Y ' || sql_id --|| ',' || sqldetail || ',' || parsing_schema_name
+from 
 (
- select force_matching_signature, max(sql_id) keep (dense_rank first order by elap_rank asc) sql_id from 
+ select force_matching_signature, max(sql_id) keep (dense_rank first order by elap_rank asc) sql_id --, sqldetail, parsing_schema_name
+ from 
 (
 SELECT * FROM(
-SELECT s.sql_id, s.force_matching_signature, 
+SELECT s.sql_id, s.force_matching_signature, --t.sqldetail ,s.PARSING_SCHEMA_NAME ,
 DENSE_RANK() OVER
       (ORDER BY sum(EXECUTIONS_DELTA) DESC ) exec_rank,
 DENSE_RANK() OVER
@@ -26,7 +28,8 @@ DENSE_RANK() OVER
       (ORDER BY sum(BUFFER_GETS_DELTA) DESC ) log_reads_rank,
 DENSE_RANK() OVER
       (ORDER BY sum(disk_reads_delta) DESC ) phys_reads_rank
- FROM dba_hist_sqlstat s,(select sql_id, from
+ FROM dba_hist_sqlstat s,(select sql_id, dbid --, sqldetail 
+                            from
                             (select
                             sql_id, dbid,  
                             REPLACE(REPLACE( dbms_lob.substr(sql_text,50,1), CHR(10) ), CHR(13) ) sqldetail 
@@ -36,20 +39,23 @@ DENSE_RANK() OVER
  WHERE s.dbid = &&ecr_dbid.  
   AND s.dbid = t.dbid
   AND s.sql_id = t.sql_id
-  AND s.PARSING_SCHEMA_NAME NOT IN ('SYS','DBSNMP','SYSMAN')
+  AND s.PARSING_SCHEMA_NAME NOT IN ('SYS','SYSTEM','DBSNMP','SYSMAN','AUDSYS')
   AND s.force_matching_signature != 0
-  GROUP BY s.sql_id,s.force_matching_signature)
+  GROUP BY s.sql_id,s.force_matching_signature --, t.sqldetail, s.PARSING_SCHEMA_NAME
+  )
 WHERE elap_rank <= &SQL_TOP_N
  OR phys_reads_rank <= &SQL_TOP_N
  or log_reads_rank <= &SQL_TOP_N
  or exec_rank <= &SQL_TOP_N
  )
-  group by force_matching_signature)
+  group by force_matching_signature --, sqldetail, parsing_schema_name
+  )
 union all
-select distinct '@planx Y ' || sql_id from 
+select distinct '@planx Y ' || sql_id --|| ',' || sqldetail || ',' || parsing_schema_name
+from 
 (
 SELECT * FROM(
-SELECT s.sql_id, s.force_matching_signature, 
+SELECT s.sql_id, s.force_matching_signature, --t.sqldetail ,s.PARSING_SCHEMA_NAME ,
 DENSE_RANK() OVER
       (ORDER BY sum(EXECUTIONS_DELTA) DESC ) exec_rank,
 DENSE_RANK() OVER
@@ -58,7 +64,8 @@ DENSE_RANK() OVER
       (ORDER BY sum(BUFFER_GETS_DELTA) DESC ) log_reads_rank,
 DENSE_RANK() OVER
       (ORDER BY sum(disk_reads_delta) DESC ) phys_reads_rank
- FROM dba_hist_sqlstat s,(select sql_id, from
+ FROM dba_hist_sqlstat s,(select sql_id, dbid --, sqldetail
+                            from
                             (select
                             sql_id, dbid,  
                             REPLACE(REPLACE( dbms_lob.substr(sql_text,50,1), CHR(10) ), CHR(13) ) sqldetail 
@@ -68,9 +75,10 @@ DENSE_RANK() OVER
  WHERE s.dbid = &&ecr_dbid.  
    AND s.dbid = t.dbid
   AND s.sql_id = t.sql_id
-  AND PARSING_SCHEMA_NAME NOT IN ('SYS','DBSNMP','SYSMAN')
+  AND PARSING_SCHEMA_NAME NOT IN ('SYS','SYSTEM','DBSNMP','SYSMAN','AUDSYS')
   AND force_matching_signature = 0
-  GROUP BY s.sql_id,s.force_matching_signature)
+  GROUP BY s.sql_id,s.force_matching_signature --, t.sqldetail, s.PARSING_SCHEMA_NAME
+  )
 WHERE elap_rank <= &SQL_TOP_N
  OR phys_reads_rank <= &SQL_TOP_N
  or log_reads_rank <= &SQL_TOP_N
