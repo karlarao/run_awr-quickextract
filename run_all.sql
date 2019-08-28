3acf7c7ee1ade5d611682ad806314ed0fb70404b
karlarao <karlarao@gmail.com>
2019-08-28 12:55:54 -0400

    skip for 18 and 19 db versions

--- run_all.sql
+++ run_all.sql
@@ -6,11 +6,11 @@
 DEF v_object_prefix = 'v$';
 DEF skip_11g_script = '';
 COL skip_11g_script NEW_V skip_11g_script;
-SELECT ' -- skip 11g ' skip_11g_column, ' echo skip 11g ' skip_11g_script FROM &&v_object_prefix.instance WHERE version LIKE '11%';
+SELECT ' -- skip 11g ' skip_11g_column, ' echo skip 11g ' skip_11g_script FROM &&v_object_prefix.instance WHERE version LIKE '11%' or version LIKE '10%';
 
 DEF skip_12c_script = '';
 COL skip_12c_script NEW_V skip_12c_script;
-SELECT ' -- skip 12c ' skip_12c_column, ' echo skip 12c ' skip_12c_script FROM &&v_object_prefix.instance WHERE version LIKE '12%';
+SELECT ' -- skip 12c ' skip_12c_column, ' echo skip 12c ' skip_12c_script FROM &&v_object_prefix.instance WHERE version LIKE '12%' or version LIKE '18%' or version LIKE '19%';
 
 
 @run_awr_topevents.sql
@@ -32,7 +32,7 @@ SELECT ' -- skip 12c ' skip_12c_column, ' echo skip 12c ' skip_12c_script FROM &
 @&&skip_11g_script.0_gvash_to_csv_12c.sql
 @run_awr_topsegments.sql
 @run_awr_planx.sql
-@run_esp_master.sql
+-- @run_esp_master.sql
 @run_awr_miner.sql
 
 
