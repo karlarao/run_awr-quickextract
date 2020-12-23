c0b5179f564d3678fc714bb5994cffff3e5349cb
karlarao <karlarao@gmail.com>
2020-12-23 14:38:22 -0500

    run_all edits

--- run_all_cdb.sql
+++ run_all_cdb.sql
@@ -34,13 +34,12 @@ SELECT ' -- skip 12c ' skip_12c_column, ' echo skip 12c ' skip_12c_script FROM &
 @run_awr_topsegments-space_used.sql
 @run_awr_planx.sql
 @run_esp_master.sql
-@run_awr_miner.sql
-@0_parsing_schema.sql
-@0_service_names.sql
 @s01_celliorm.sql
 @s01_cellver.sql
 @s05_obj_size.sql
 @s10_hcc_tables_ts.sql
-
+@0_parsing_schema.sql
+@0_service_names.sql
+@run_awr_miner.sql
 
 exit
